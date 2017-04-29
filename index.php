c8f63bb651c297240c286b05658f04fa1c4c596f
mmeezan1987 <mizanrhn1@gmail.com>
2017-04-29 22:08:02 +0600

    file upload

--- index.php
+++ index.php
@@ -4,12 +4,21 @@
 
         <div class="coding">
             <?php
-            $ourfile = fopen("text.txt", "r") or die('File is not found');
-            while(!feof($ourfile)){
-                echo fgets($ourfile)."<br/>";
-            };
-            fclose($ourfile);
+
+            if(isset($_FILES['image'])){
+                $file = $_FILES['image']['name'];
+                $filetmp = $_FILES['image']['tmp_name'];
+                move_uploaded_file($filetmp,'images/'.$file);
+
+                echo "File upload successfully";
+            }
+
             ?>
+
+            <form action="index.php" method="post" enctype="multipart/form-data">
+                <input type="file" name="image"/><br/>
+                <input type="submit" value="Submit"/>
+            </form>
 
         </div>
 
