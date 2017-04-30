0f08248965fef3cee079c3fcbd860fcf2b459ae6
mmeezan1987 <mizanrhn1@gmail.com>
2017-04-30 13:43:24 +0600

    Folder name change

--- index.php
+++ index.php
@@ -4,22 +4,19 @@
 
         <div class="coding">
             <?php
+            if(isset($_FILES['file'])){
+                $file = $_FILES['file']['name'];
+                $filetmp = $_FILES['file']['tmp_name'];
+                move_uploaded_file($filetmp,'uploads/'.$file);
 
-            if(isset($_FILES['image'])){
-                $file = $_FILES['image']['name'];
-                $filetmp = $_FILES['image']['tmp_name'];
-                move_uploaded_file($filetmp,'images/'.$file);
-
-                echo "File upload successfully";
+                echo "File is uploaded successfully";
             }
 
             ?>
-
             <form action="index.php" method="post" enctype="multipart/form-data">
-                <input type="file" name="image"/><br/>
+                <input type="file" name="file"/><br/>
                 <input type="submit" value="Submit"/>
             </form>
-
         </div>
 
 
