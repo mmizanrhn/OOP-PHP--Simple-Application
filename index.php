6ce5578a7367ed3fa6e3f110157bad9e47feeed1
mmeezan1987 <mizanrhn1@gmail.com>
2017-05-02 19:40:12 +0600

    Multidimentional array

--- index.php
+++ index.php
@@ -13,11 +13,19 @@ include("header.php");
             for($i=0;$i<$lentgh;$i++){
                 echo $cars[$i]."<br/>";
             }
-            // Multi associative array
+            // Associative array
             $age = array("Abdullah"=> "30","Mahmud"=>"33","Jamal"=>"28","Habib"=>"25");
             foreach($age as $key => $value){
                 echo "Name -".$key." "."Age -".$value."<br/>";
             }
+
+            // Multidimentional array
+            $students = array(
+                array("Masum","Khalid","Zamil"),
+                array("Khairul","Kabir","Bashar"),
+                array("Jakir","Kamal","Monir")
+            );
+            echo $students['0']['2'];
 
             ?>
 
