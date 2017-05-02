73dc308977dd52bdcb4c0de189af3b2ef558c58d
mmeezan1987 <mizanrhn1@gmail.com>
2017-05-03 00:21:38 +0600

    Array_column

--- index.php
+++ index.php
@@ -7,38 +7,29 @@ include("header.php");
 
             <?php
 
-            // INDEX ARRAY
-            $cars = array("Volbo","Toyota","Maruti","Scania");
-            $lentgh = count($cars);
-            echo $lentgh;
-            // Associative array
-            $age = array("Abdullah"=> "30","Mahmud"=>"33","Jamal"=>"28","Habib"=>"25");
-            foreach($age as $key => $value){
-                echo "Name -".$key." "."Age -".$value."<br/>";
-            }
-
-            // Multidimentional array
-            $students = array(
-                array("Masum","Khalid","Zamil"),
-                array("Khairul","Kabir","Bashar"),
-                array("Jakir","Kamal","Monir")
+            $employess = array(
+                array(
+                    "id"         => "301" ,
+                    "first_name" => "Khalid",
+                    "last_name"  => "Zamil"
+                ),
+                array(
+                    "id"         => "402",
+                    "first_name" => "Kamal",
+                    "last_name"  => "Hossain"
+                ),
+                array(
+                    "id"         => "401",
+                    "first_name" => "Mizan",
+                    "last_name"  => "Rahman"
+                )
             );
-            echo $students['0']['2']."<br/>";
+           sort($employess);
+           print_r("<pre>");
+           print_r(array_column($employess,'last_name','id'));
+           print_r("</pre>");
 
-            // array_change_key_case() // built in function
-            $ages = array(
-                "ABDullah" => "30",
-                "MahMud"   => "33",
-                "jamal"    => "28",
-                "habib"    => "25"
-            );
-
-            print_r("<pre>");
-            print_r(array_change_key_case($ages,CASE_UPPER));
-            print_r("</pre>");
-
-
-            ?>
+           ?>
 
         </div>
 
