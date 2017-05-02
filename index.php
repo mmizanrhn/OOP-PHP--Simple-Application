4687c1f66ad4234bd04a9c92f5583549a7f18851
mmeezan1987 <mizanrhn1@gmail.com>
2017-05-02 19:15:02 +0600

    Create a index array

--- index.php
+++ index.php
@@ -6,12 +6,14 @@ include("header.php");
         <div class="coding">
 
             <?php
-            $fruits = array (
-                "fruits"  => array("a" => "orange", "b" => "banana", "c" => "apple"),
-                "numbers" => array(1, 2, 3, 4, 5, 6),
-                "holes"   => array("first", 5 => "second", "third")
-            );
-            echo $fruits['fruits']['c'];
+
+            // INDEX ARRAY
+            $cars = array("Volbo","Toyota","Maruti","Scania");
+            $lentgh = count($cars);
+            for($i=0;$i<$lentgh;$i++){
+                echo $cars[$i]."<br/>";
+            }
+
             ?>
 
         </div>
