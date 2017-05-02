827ad1e7adf0983e3def2cd73dcc8e632ed2e7fd
mmeezan1987 <mizanrhn1@gmail.com>
2017-05-02 19:30:17 +0600

    Create a multidimentional array

--- index.php
+++ index.php
@@ -13,6 +13,11 @@ include("header.php");
             for($i=0;$i<$lentgh;$i++){
                 echo $cars[$i]."<br/>";
             }
+            // Multi associative array
+            $age = array("Abdullah"=> "30","Mahmud"=>"33","Jamal"=>"28","Habib"=>"25");
+            foreach($age as $key => $value){
+                echo "Name -".$key." "."Age -".$value."<br/>";
+            }
 
             ?>
 
