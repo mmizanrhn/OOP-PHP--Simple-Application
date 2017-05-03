9bbe794e9776c12d222182e7fb15f5993376a87c
mmeezan1987 <mizanrhn1@gmail.com>
2017-05-04 00:09:35 +0600

    Smimple form validation

--- form-process.php
+++ form-process.php
@@ -1,1 +1,17 @@
-// Somehting to do //
+<pre>
+    <?php
+        print_r($_POST);
+    ?>
+</pre>
+<?php
+if(isset($_POST['username']) && isset($_POST['password']) && !empty($_POST['username']) && !empty($_POST['password'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    echo "Username : ".$username."<br/>";
+    echo "Password : ".$password;
+}else{
+    header('location:form.php');
+}
+
+
+?>
