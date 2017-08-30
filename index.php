deeecdb86ff5d8922586298301f66baeeebe31c4
mmeezan1987 <mizanrhn1@gmail.com>
2017-08-30 16:14:20 +0600

    logical expression

--- index.php
+++ index.php
@@ -6,19 +6,33 @@ include("header.php");
         <div class="coding">
 
             <?php
-
-            $cars = array(
-                "color" => "Black",
-                "id"    => "201",
-                "name"  => "Bmw",
-                "company"=>"Nitol"
-            );
-
-            foreach($cars as $car => $value){
-				echo $value. "<br>";
-			}
-
-           ?>
+			    
+				$a = 5;
+				$b = 8;
+				$c = 7;
+				$d = 20;
+				// This is logical expression primary example 
+				if( ($a <= $b) && ($d >= $c)){
+					echo "B is larger than A AND". "<br>";
+					echo "C is smaller than D";
+				}
+				
+				echo "<br/>";
+				
+				// This is logical expression example 
+				$e = 100;
+				if(!isset($e)){
+					$e = 200;
+				}
+				echo $e."<br>";
+				
+				// This is logical expression practical example 
+				$quantity = "";
+				if( empty($quantity) && !is_numeric($quantity) ){
+					echo "Please input your quantity here";
+				}
+				
+			?>
 
         </div>
 
